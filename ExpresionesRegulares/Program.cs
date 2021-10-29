91ae8e74f1554bd9ee20e573edce201a0aa5eb8d
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-10-29 18:09:00 +0200

    Nuevo

---
+++ ExpresionesRegulares/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExpresionesRegulares
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string frase = "Mi web es: https://www.instagram.com/vintagenewlifeclothing/";
+
+			string patron = @"http(s)?://(www.)?instagram.com";
+
+			Regex regex = new Regex(patron);
+
+			MatchCollection mc = regex.Matches(frase);
+
+			if (mc.Count > 0)
+				Console.WriteLine("Se ha encontrado");
+			else
+				Console.WriteLine("No Se ha encontrado");
+		}
+	}
+}
